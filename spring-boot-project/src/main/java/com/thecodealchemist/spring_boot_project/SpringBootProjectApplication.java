e24ef79252bf17a923a27e8e1b16cc567be7a1c0
Suhani-Khosla <suhanilife0505@gmail.com>
2025-09-23 23:20:16 +0530

    blu

--- spring-boot-project/src/main/java/com/thecodealchemist/spring_boot_project/SpringBootProjectApplication.java
+++ spring-boot-project/src/main/java/com/thecodealchemist/spring_boot_project/SpringBootProjectApplication.java
@@ -1,7 +1,12 @@
 package com.thecodealchemist.spring_boot_project;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.thecodealchemist.spring_boot_project.service.StudentService;
 
 @SpringBootApplication
 public class SpringBootProjectApplication {
@@ -9,5 +14,13 @@ public class SpringBootProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProjectApplication.class, args);
 	}
+
+	// @Bean
+    // public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+    //     return args -> {
+    //         StudentService studentService = ctx.getBean(StudentService.class);
+    //         studentService.fetchstudent();
+    //     };
+    // }
 
 }
